811f4da5f7c65c723b280583e954066b8eead562
Betül Toyoğlu <toyoglubetul@gmail.com>
2025-02-03 16:50:24 +0300

    insert,update,delete,search

--- app/src/main/java/com/example/contacts_app_with_kotlin/data/datasource/KisilerDataSource.kt
+++ app/src/main/java/com/example/contacts_app_with_kotlin/data/datasource/KisilerDataSource.kt
@@ -14,22 +14,22 @@ class KisilerDataSource(var kdao:KisilerDao) {
         }
 
     suspend fun kaydet(kisi_ad:String, kisi_tel:String){
-        Log.e("kişi kaydet", "$kisi_ad - $kisi_tel")
+        val yeniKisi = Kisiler(0,kisi_ad,kisi_tel)
+        kdao.kaydet(yeniKisi)
     }
 
     suspend fun guncelle(kisi_id:Int, kisi_ad:String, kisi_tel:String){
-        Log.e("kişi güncelle", "$kisi_id - $kisi_ad - $kisi_tel")
+        val guncellenenKisi = Kisiler(kisi_id,kisi_ad,kisi_tel)
+        kdao.guncelle(guncellenenKisi)
     }
 
     suspend fun sil (kisi_id:Int){
-        Log.e("kişi sil", kisi_id.toString())
+        val silinenKisi = Kisiler(kisi_id, "", "")
+        kdao.sil(silinenKisi)
     }
 
     suspend fun ara(aramaKelimesi:String): List<Kisiler> =
         withContext(Dispatchers.IO){
-            val kisilerListesi = ArrayList<Kisiler>()
-            val k1 = Kisiler(1, "ahmet", "222")
-            kisilerListesi.add(k1)
-            return@withContext kisilerListesi
+            return@withContext kdao.ara(aramaKelimesi)
         }
 }
